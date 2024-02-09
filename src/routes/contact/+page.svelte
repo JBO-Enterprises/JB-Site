90e862c24536c8084b57ae1aa037ffee86a5e542
JB Stepan <29986350+JBStepan@users.noreply.github.com>
2024-02-09 13:33:29 -0600

    Commented out contact, still geting error from Cloudflare

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -2,70 +2,84 @@
   import NavBar from "../../components/NavBar.svelte";
   import { redirect } from "@sveltejs/kit";
 
-  let name: string
-  let email: string
-  let content: string
-  let taken: boolean = false
+  // let name: string;
+  // let email: string;
+  // let content: string;
+  // let taken: boolean = false;
 
-  async function submitForm(){
-    const data = {"name": name, "email": email, "content": content, "date": Date.now()}
-    const res = await fetch("https://api.jbstepan.com/contact/create", { method: "POST", body: JSON.stringify(data)})
+  // async function submitForm() {
+  //   const data = { name: name, email: email, content: content };
+  //   const res = await fetch("https://api.jbstepan.com/contact/create", {
+  //     method: "POST",
+  //     headers: { "Content-Type": "application/json", "Access-Control-Allow-Origin": "*" },
+  //     body: JSON.stringify(data),
+  //   });
 
-    if(res.status == 200) {
-      console.log("200")
-    } else if(res.status == 401) {
-      console.log("401")
-    } else if (res.status == 500) {
-      console.log("500")
-    }
-  }
+  //   if (res.status === 200) {
+  //     alert("200");
+  //   } else if (res.status === 400) {
+  //     alert("400");
+  //   } else if (res.status === 401) {
+  //     alert("401");
+  //   } else if (res.status === 500) {
+  //     alert("500");
+  //   }
+  // }
 </script>
 
 <svelte:head>
   <title>Contact</title>
 </svelte:head>
 
-<main>
+<!-- <main>
   <NavBar />
   <h1>Contact Form</h1>
   <center>
-    <form on:submit|preventDefault={submitForm}>
-      <input type="text" placeholder="EMail" bind:value={email}><br><br>
-      <input type="text" placeholder="Name" bind:value={name}><br><br>
-      <textarea cols="30" rows="10" placeholder="What would you like to contact about?" bind:value={content}></textarea><br><br>
-      <input type="submit" class="submit" value="Submit">
+    <form on:submit={submitForm}>
+      <input type="text" placeholder="EMail" bind:value={email} /><br /><br />
+      <input type="text" placeholder="Name" bind:value={name} /><br /><br />
+      <textarea
+        cols="30"
+        rows="10"
+        placeholder="What would you like to contact about?"
+        bind:value={content}
+      /><br /><br />
+      <button type="submit">Submit</button>
     </form>
   </center>
+</main> -->
+
+<main>
+  <h1>Currently down! 🤒</h1>
 </main>
 
 <style>
-    :root {
-      font-family: Inter, system-ui, Avenir, Helvetica, Arial, sans-serif;
-      line-height: 1.5;
-      font-weight: 400;
-  
-      color-scheme: light dark;
-      color: rgba(255, 255, 255, 0.87);
-      background-color: #242424;
-  
-      font-synthesis: none;
-      text-rendering: optimizeLegibility;
-      -webkit-font-smoothing: antialiased;
-      -moz-osx-font-smoothing: grayscale;
-      -webkit-text-size-adjust: 100%;
-    }
-    
-    main {
-      text-align: center;
-      margin-top: 6.5rem;
-    }
-  
+  :root {
+    font-family: Inter, system-ui, Avenir, Helvetica, Arial, sans-serif;
+    line-height: 1.5;
+    font-weight: 400;
+
+    color-scheme: light dark;
+    color: rgba(255, 255, 255, 0.87);
+    background-color: #242424;
+
+    font-synthesis: none;
+    text-rendering: optimizeLegibility;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-text-size-adjust: 100%;
+  }
+  /*  main {
+    text-align: center;
+    margin-top: 6.5rem;
+  }
+
   .submit {
     transition: 300ms;
     transform: scale(1);
     background-color: rgb(188, 188, 188);
     color: black;
-    padding: .7rem;
+    padding: 0.7rem;
     outline: transparent;
     border: transparent;
     font-size: 25px;
@@ -87,7 +101,7 @@
   textarea {
     font-family: Inter, system-ui, Avenir, Helvetica, Arial, sans-serif;
     padding: 6px 12px;
-    margin-bottom: .5rem;
+    margin-bottom: 0.5rem;
     background-color: rgb(188, 188, 188);
     color: black;
     border: 1px solid transparent;
@@ -108,6 +122,5 @@
     padding: 6px 16px;
     max-width: max-content;
   }
-
-</style>
-  
+  */
+</style> 
